2e832ad12305cfbfd71a33628779c2acf44ac7cd
Gasook Jung <gasookjung@Gasooks-MacBook-Pro.local>
2020-11-13 14:08:16 -0800

    fixed chart 3

--- scripts/chart_3.R
+++ scripts/chart_3.R
@@ -3,34 +3,28 @@
 library("ggplot2")
 library("dplyr")
 
+#set working directory first
 avg_boston <- info_summary(boston)$price_by_num_accomodates
 avg_chicago <- info_summary(chicago)$price_by_num_accomodates
 avg_singapore <- info_summary(singapore)$price_by_num_accomodates
 
-
-#compare_boston <- ggplot(data = avg_boston) +
- # geom_line(mapping = aes(x = accommodates, y = avg_price))
-
-#compare_chicago <- ggplot(data = avg_chicago)+
- # geom_line(mapping = aes(x = accommodates, y = avg_price))
-
-#compare_singapor <- ggplot(data = avg_singapore)+
- # geom_line(mapping = aes(x = accommodates, y = avg_price))
-
-
 colors <- c("Boston" = "blue", "Chicago" = "red", "Singapore" = "green")
 
+#create a line plot showing the average prices for each city based on accommodation
 compare_all <- ggplot() +
   geom_line(data = avg_boston, aes(x = accommodates, y = avg_price, color = "Boston")) +
   geom_line(data = avg_chicago, aes(x = accommodates, y = avg_price, color = "Chicago")) +
   geom_line(data = avg_singapore, aes(x = accommodates, y = avg_price, color = "Singapore")) +
+  scale_x_continuous(breaks = seq(0, 17, 1)) +
+  geom_point() +
   #geom_point(mapping = aes(x = accommodates, y = avg_price)) +
-  #how to make it show points and for the x-axis to be separated by 1?
-  #also how to create a legend 
+  #how to make it show points
+  #also how to lint???
   labs(title = "Average Price for Number of Accomodations in Different Cities",
        x = "Number of People",
        y = "Average Price",
        color = "Legend") +
-  scale_color_manual(values = colors)
+  scale_color_manual(values = colors) 
+  
 
 
