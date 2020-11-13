2d8b4dcaac40ca4f06ee1fde6c081960708505bc
Miranda Ma <majiayi0319@gmail.com>
2020-11-13 02:00:30 -0800

    pie chart for room types in Boston

---
+++ scripts/chart_1.R
@@ -0,0 +1,17 @@
+library(ggplot2)
+library(tidyverse)
+# set working directory first
+boston <- read.csv("data/boston_listings.csv")
+
+# extracts the room_type column and counts the numbers of each type
+type <- boston %>%
+  group_by(room_type) %>%
+  summarize(num_type = n())
+
+# creates a pie chart of room types on Airbnb in Boston
+ggplot(data = type) +
+  geom_bar(mapping = aes(x = "", y = num_type, fill = room_type), stat="identity") +
+  coord_polar("y") +
+  theme_void() +
+  labs(title = "Types of Rooms on Airbnb in Boston") +
+  geom_text(mapping = aes(x = "", y = num_type, label = num_type))
