cdec6d2a51697096782d524ab8ff7753ccba987e
Cole Palacios <coleap@uw.edu>
2020-11-12 18:44:33 -0800

    function creates summary list

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -9,41 +9,52 @@ chicago$price <- as.numeric(gsub('[$,]', '', chicago$price))
 london$price <- as.numeric(gsub('[$,]', '', london$price))
 
 
-boston_summary <- list()
-# number of listings
-# median price
-# mean bedrooms
-# superhost ratio
-# number of neighborhoods
-# listings in each neighborhood
-# price by accommodation
-# price by neighborhood
-boston_summary$num_listings <- nrow(boston)
-boston_summary$median_price <- median(boston$price)
-boston_summary$mean_bedrooms <- round(mean(boston$bedrooms, na.rm = 1), digits = 2)
-boston_summary$superhost_ratio <- boston %>% 
-  filter(host_is_superhost == "t") %>% 
-  summarise(superhost_ratio = round(length(host_is_superhost)
-                                    / boston_summary$num_listings, 
-                                    digits = 2)) %>% 
-  pull(superhost_ratio)
-boston_summary$num_neighborhoods <- n_distinct(boston$neighbourhood_cleansed)
-boston_summary$num_in_neighborhood <- boston %>% 
-  group_by(neighbourhood_cleansed) %>% 
-  rename(neighborhood = neighbourhood_cleansed) %>% 
-  count(neighborhood) %>% 
-  arrange(desc(n))
-boston_summary$price_by_num_accomodates <- boston %>% 
-  group_by(accommodates) %>% 
-  summarise(avg_price = mean(price))
-boston_summary$price_by_neighborhood <- boston %>% 
-  group_by(neighbourhood_cleansed) %>% 
-  summarise(avg_price = round(mean(price), digits = 2),
-            min_price = min(price),
-            max_price = max(price))
-
-
-
-
-
+# pass each dataset into the function to return an summary list
+info_summary <- function(dataset) {
+  # number of listings
+  # median price
+  # mean bedrooms
+  # superhost ratio
+  # number of neighborhoods
+  # listings in each neighborhood
+  # price by accommodation
+  # price by neighborhood
+  # property type by neighborhood
+  num_listings <- nrow(dataset)
+  median_price <- median(dataset$price)
+  mean_bedrooms <- round(mean(dataset$bedrooms, na.rm = 1), digits = 2)
+  superhost_ratio <- dataset %>% 
+    filter(host_is_superhost == "t") %>% 
+    summarise(superhost_ratio = round(length(host_is_superhost)
+                                      / dataset$num_listings, 
+                                      digits = 2)) %>% 
+    pull(superhost_ratio)
+  num_neighborhoods <- n_distinct(dataset$neighbourhood_cleansed)
+  num_in_neighborhood <- dataset %>% 
+    group_by(neighbourhood_cleansed) %>% 
+    rename(neighborhood = neighbourhood_cleansed) %>% 
+    count(neighborhood) %>% 
+    arrange(desc(n))
+  price_by_num_accomodates <- dataset %>% 
+    group_by(accommodates) %>% 
+    summarise(avg_price = mean(price))
+  price_by_neighborhood <- dataset %>% 
+    group_by(neighbourhood_cleansed) %>% 
+    summarise(min_price = min(price),
+              avg_price = round(mean(price), digits = 2),
+              max_price = max(price))
+  property_type <- dataset %>% 
+    group_by(neighbourhood_cleansed) %>% 
+    count(property_type)
+  summary_dataset <- list (num_listings = num_listings, 
+                           median_price = median_price, 
+                           mean_bedrooms = mean_bedrooms,
+                           superhost_ratio = superhost_ratio, 
+                           num_neighborhoods = num_neighborhoods, 
+                           num_in_neighborhood = num_in_neighborhood, 
+                           price_by_num_accomodates = price_by_num_accomodates, 
+                           price_by_neighborhood = price_by_neighborhood, 
+                           property_type = property_type)
+  summary_dataset
+}
 
