126de558d0e6111300cd2d68cd77ee0a5b4c41c4
Cole Palacios <coleap@uw.edu>
2020-11-13 19:06:45 -0800

    lint & style edits

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -6,9 +6,9 @@ boston <- read.csv("data/boston_listings.csv")
 chicago <- read.csv("data/chicago_listings.csv")
 singapore <- read.csv("data/singapore_listings.csv")
 
-boston$price <- as.numeric(gsub('[$,]', '', boston$price))
-chicago$price <- as.numeric(gsub('[$,]', '', chicago$price))
-singapore$price <- as.numeric(gsub('[$,]', '', singapore$price))
+boston$price <- as.numeric(gsub("[$,]", "", boston$price))
+chicago$price <- as.numeric(gsub("[$,]", "", chicago$price))
+singapore$price <- as.numeric(gsub("[$,]", "", singapore$price))
 
 
 # pass each dataset into the function to return a summary list
@@ -25,38 +25,43 @@ info_summary <- function(dataset) {
   num_listings <- nrow(dataset)
   median_price <- median(dataset$price)
   mean_bedrooms <- round(mean(dataset$bedrooms, na.rm = 1), digits = 2)
-  superhost_ratio <- dataset %>% 
-    filter(host_is_superhost == "t") %>% 
+  superhost_ratio <- dataset %>%
+    filter(host_is_superhost == "t") %>%
     summarise(superhost_ratio = round(length(host_is_superhost)
-                                      / dataset$num_listings, 
-                                      digits = 2)) %>% 
+    / dataset$num_listings,
+    digits = 2
+    )) %>%
     pull(superhost_ratio)
   num_neighborhoods <- n_distinct(dataset$neighbourhood_cleansed)
-  num_in_neighborhood <- dataset %>% 
-    group_by(neighbourhood_cleansed) %>% 
-    rename(neighborhood = neighbourhood_cleansed) %>% 
-    count(neighborhood) %>% 
+  num_in_neighborhood <- dataset %>%
+    group_by(neighbourhood_cleansed) %>%
+    rename(neighborhood = neighbourhood_cleansed) %>%
+    count(neighborhood) %>%
     arrange(desc(n))
-  price_by_num_accomodates <- dataset %>% 
-    group_by(accommodates) %>% 
+  price_by_num_accomodates <- dataset %>%
+    group_by(accommodates) %>%
     summarise(avg_price = round(mean(price), digits = 2))
-  price_by_neighborhood <- dataset %>% 
-    group_by(neighbourhood_cleansed) %>% 
-    summarise(min_price = min(price),
-              avg_price = round(mean(price), digits = 2),
-              max_price = max(price))
-  property_type <- dataset %>% 
-    group_by(neighbourhood_cleansed) %>% 
+  price_by_neighborhood <- dataset %>%
+    group_by(neighbourhood_cleansed) %>%
+    summarise(
+      min_price = min(price),
+      avg_price = round(mean(price), digits = 2),
+      max_price = max(price)
+    )
+  property_type <- dataset %>%
+    group_by(neighbourhood_cleansed) %>%
     count(property_type)
-  summary_dataset <- list (num_listings = num_listings, 
-                           median_price = median_price, 
-                           mean_bedrooms = mean_bedrooms,
-                           superhost_ratio = superhost_ratio, 
-                           num_neighborhoods = num_neighborhoods, 
-                           num_in_neighborhood = num_in_neighborhood, 
-                           price_by_num_accomodates = price_by_num_accomodates, 
-                           price_by_neighborhood = price_by_neighborhood, 
-                           property_type = property_type)
+  summary_dataset <- list(
+    num_listings = num_listings,
+    median_price = median_price,
+    mean_bedrooms = mean_bedrooms,
+    superhost_ratio = superhost_ratio,
+    num_neighborhoods = num_neighborhoods,
+    num_in_neighborhood = num_in_neighborhood,
+    price_by_num_accomodates = price_by_num_accomodates,
+    price_by_neighborhood = price_by_neighborhood,
+    property_type = property_type
+  )
   summary_dataset
 }
 
