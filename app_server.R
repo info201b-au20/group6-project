863fb3a41047a294d8b99e42205b38ea8e2d6e62
jwc225 <jwchang225@gmail.com>
2020-12-08 07:11:55 -0800

    complete interactive page 1

--- app_server.R
+++ app_server.R
@@ -3,45 +3,77 @@ library("shiny")
 library("tidyverse")
 library("leaflet")
 library("RColorBrewer")
-library("htmltools")
+library("batman")
 
 # Read data (set wdir to root)
 data_sing <- read.csv("data/singapore_listings.csv")
 
-# Define a server for the application
 
-server <- function(input, output) {
-  #---- Page one --------------------------------------------------------------
-  
+# Define a server for the application
+server <- function(input, output, session) {
+
+  ##### Interactive Page One ##################################################
+
+  # Change max price range when button clicked
+  observeEvent(input$button, {
+    max <- input$textbox
+    updateSliderInput(session, "price_slider", max = max)
+    updateTextInput(session, "textbox", value = "") # clear input after click
+  })
+
+  # Construct a color palette (scale) based on the `room-type` column
   palette_fn <- colorFactor(palette = "Dark2", domain = data_sing$room_type)
-  
+
+  # Replace price column with a vector of numbers
+  data_sing$price <- as.numeric(gsub("[$,]", "", data_sing$price))
+
+  # Replace superhost column with boolean values
+  data_sing$host_is_superhost <- to_logical(data_sing$host_is_superhost)
+
   output$my_map <- renderLeaflet({
-    
-    leaflet(data = data_sing) %>%
+
+    # Dynamic user filtering
+    plot_data <- data_sing %>%
+      filter(price >= input$price_slider[1] &
+        price <= input$price_slider[2]) %>%
+      filter(accommodates >= input$accom_slider) %>%
+      filter(if (input$checkbox == TRUE) host_is_superhost == TRUE
+             else id == id) %>%
+      filter(if (input$select == "All") id == id
+             else neighbourhood_cleansed == input$select)
+
+    # Return the count of filtered listings
+    filter_count <- nrow(plot_data)
+
+    # Create Leaflet map of user-filtered Singapore listings
+    leaflet(data = plot_data) %>%
       addProviderTiles("CartoDB.Positron") %>%
-      setView(lng = 103.851959, lat = 1.290270, zoom = 11) %>%
+      setView(lng = 103.851959, lat = 1.290270, zoom = 10.5) %>%
       addCircles(
         lat = ~latitude,
         lng = ~longitude,
         stroke = F,
-        popup = ~listing_url,
-        color = ~palette_fn(room_type),
+        popup = ~ paste(name, listing_url),
+        color = ~ palette_fn(room_type),
         radius = 20,
         fillOpacity = 0.5
-      ) %>% 
+      ) %>%
       addLegend(
         position = "bottomright",
-        title = "Airbnb Listings in Singapore",
+        title = paste0(
+          "Airbnb Listings in Singapore (",
+          paste0(filter_count, " results)")
+        ),
         pal = palette_fn,
         values = ~room_type,
         opacity = 1
       )
   })
-  
-  #---- Page two --------------------------------------------------------------
-  
-  
-  
-  #---- Page three ------------------------------------------------------------
-  
+
+
+  ##### Interactive Page Two ##################################################
+
+
+
+  ##### Interactive Page Three ################################################
 }
--- app_ui.R
+++ app_ui.R
@@ -2,41 +2,108 @@
 library("shiny")
 library("tidyverse")
 library("leaflet")
-library("RColorBrewer")
 
 # Read data (set wdir to root)
 data_sing <- read.csv("data/singapore_listings.csv")
 
 
-#---- Introduction ------------------------------------------------------------
+#### Introduction #############################################################
 
 intro <- tabPanel(
   title = tags$h4("Introduction")
 )
 
 
-#---- Interactive page one ----------------------------------------------------
+##### Interactive Page One ####################################################
 
+# Convert price column to vector of numbers
+data_sing$price <- as.numeric(gsub("[$,]", "", data_sing$price))
+
+
+# Set default max slider range for better control (as opposed to the
+# max price from the dataset due to heavy left skew distribution)
+range_min <- min(data_sing$price)
+range_max <- 500
+
+slider_price <- sliderInput(
+  inputId = "price_slider",
+  label = "Listing Price ($SGD per night)",
+  min = range_min,
+  max = range_max,
+  sep = ",",
+  pre = "$",
+  value = c(range_min, range_max),
+  dragRange = TRUE
+)
+
+# Allow user to set new max price range
+change_max_range <- textInput(
+  inputId = "textbox",
+  label = tags$h6("Set max slider range")
+)
+
+slider_accomodates <- sliderInput(
+  inputId = "accom_slider",
+  label = "Guest capacity (at least as high)",
+  min = min(data_sing$accommodates),
+  max = max(data_sing$accommodates),
+  step = 1,
+  value = 1
+)
+
+# Sort neigbhourhoods in order of num listings
+sorted <- data_sing %>%
+  group_by(neighbourhood_cleansed) %>%
+  summarize(num_listings = n()) %>%
+  arrange(desc(num_listings))
+
+select_neigbourhood <- selectInput(
+  inputId = "select",
+  label = "Select  neigbourhood",
+  choices = c("All", sorted$neighbourhood_cleansed)
+)
+
+checkbox_superhost <- checkboxInput(
+  inputId = "checkbox",
+  label = tags$strong("Superhost Listings Only")
+)
+
+
+# define a layout for first interactive page
 page_one <- tabPanel(
   title = tags$h4("Interactive Map"),
-  sidebarLayout(
-    sidebarPanel(
-    ),
-    mainPanel(
-      leafletOutput("my_map")
-    )
+  tags$h2("Singapore Airbnb Locations"),
+  leafletOutput("my_map"),
+  hr(),
+  h3("Filter Options"),
+  br(),
+  fluidRow(
+    column(4,
+           slider_price,
+           change_max_range,
+           actionButton("button", "Change")
+           ),
+    column(4,
+           slider_accomodates,
+           ),
+    column(4,
+           select_neigbourhood,
+           br(),
+           checkbox_superhost)
   )
 )
 
-#---- Interactive page two ----------------------------------------------------
+
+
+##### Interactive Page Two ####################################################
 
 
 
-#---- Interactive page three --------------------------------------------------
+##### Interactive Page Three ##################################################
 
 
 
-#---- Summary -----------------------------------------------------------------
+##### Summary #################################################################
 
 
 
@@ -44,7 +111,7 @@ page_one <- tabPanel(
 
 # Define a ui for the application
 ui <- navbarPage(
-  title = "Airbnb Data Exploration",
+  title = tags$strong("Airbnb Data Exploration"),
   intro,
   page_one
 )
