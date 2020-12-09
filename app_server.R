817857ba1e7c90d17dbc037cbfd79b4f2986ec4f
jwc225 <jwchang225@gmail.com>
2020-12-09 01:17:39 -0800

    adjust map zoom, longitude

--- app_server.R
+++ app_server.R
@@ -54,7 +54,7 @@ server <- function(input, output, session) {
         "05aa38b3c8/{z}/{x}/{y}.png?access-token=eIlOZCXWfZIR2t5pqcGt6vcc25pb",
         "scLwwCKzFgtOjISymDP6p3nvlwwLl4mA0qeH"),
       ) %>%
-      setView(lng = 103.851959, lat = 1.3521, zoom = 11) %>%
+      setView(lng = 103.841959, lat = 1.3521, zoom = 11.5) %>%
       addCircles(
         lat = ~latitude,
         lng = ~longitude,
