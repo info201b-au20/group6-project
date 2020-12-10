f62a12938baed31e8f1c25744feb5eca2d8001da
jwc225 <jwchang225@gmail.com>
2020-12-09 18:25:05 -0800

    lint and fix encoding issue w/ popup text

--- app_server.R
+++ app_server.R
@@ -38,56 +38,74 @@ server <- function(input, output, session) {
     plot_data <- data_sing %>%
       filter(price >= input$price_slider[1] &
         price <= input$price_slider[2]) %>%
-      filter(accommodates >= input$accom_slider) %>%
-      filter(if (input$checkbox == TRUE) host_is_superhost == TRUE
-             else id == id) %>%
-      filter(if (input$select == "All") id == id
-             else neighbourhood_cleansed == input$select)
+      filter(accommodates >= input$accom_slider[1] &
+        accommodates <= input$accom_slider[2]) %>%
+      filter(if (input$checkbox == TRUE)
+        host_is_superhost == TRUE
+        else id == id) %>%
+      filter(if (input$select == "All")
+        id == id
+        else neighbourhood_cleansed == input$select)
 
     # Get the count of filtered listings
     filter_count <- nrow(plot_data)
-    
+
     # Get map pop-up content for listing rating
-    popup_rating <- ifelse(is.numeric(plot_data$review_scores_rating),
-                           paste0("<b style='color:#FF5A5F;'>&#9733; ",
-                                plot_data$review_scores_rating * 0.05,
-                                "</b> (",
-                                plot_data$number_of_reviews, ")"),
-                           "No Reviews")
-    
+    popup_rating <- ifelse(plot_data$number_of_reviews > 0,
+      paste0(
+        "<b style='color:#FF5A5F;'>&#9733; ",
+        plot_data$review_scores_rating * 0.05,
+        "</b> (",
+        plot_data$number_of_reviews, ")"
+      ),
+      "No Reviews"
+    )
+
     # Get map pop-up content for host status
-    popup_superhost <- ifelse(plot_data$host_is_superhost == T, 
-                              paste0(" · <b style='color:#FF5A5F;'>
+    popup_superhost <- ifelse(plot_data$host_is_superhost == T,
+      paste0(" &#183; <b style='color:#FF5A5F;'>
                                      &#127894;</b> Superhost"),
-                              "")
-    
+      ""
+    )
+
     # Get map pop-up content for guest capacity
-    popup_guests <- ifelse(plot_data$accommodates > 1, 
-                         paste0(plot_data$accommodates, " guests"), 
-                         paste0(plot_data$accommodates, " guest")
-                         )
-    
+    popup_guests <- ifelse(plot_data$accommodates > 1,
+      paste0(plot_data$accommodates, " guests"),
+      paste0(plot_data$accommodates, " guest")
+    )
+
     # Compile all content for map pop-up
-    popup_content <- paste0(sep = "<br/>",
-           paste0("<h5><span style='color:#767676;'>",
-                  popup_rating, popup_superhost,
-                  " · <u>", plot_data$neighbourhood_cleansed, 
-                  ", Singapore</u></span></h5><hr>"),
-           paste0("<center><h4><b>$", plot_data$price,
-                  "</b> / night</h4></center>"),
-           paste0("<center><h6>", popup_guests, "</h6></center>"),
-           paste0("<center><h5><b><a href=", plot_data$listing_url,
-                  ">", plot_data$name, "</a></b></h5></center>"),
-           paste0("<center><img src=", plot_data$picture_url,
-                  " width=300 height=180></center>")
+    popup_content <- paste0(
+      sep = "<br/>",
+      paste0(
+        "<h5><span style='color:#767676;'>",
+        popup_rating, popup_superhost,
+        " &#183; <u>", plot_data$neighbourhood_cleansed,
+        ", Singapore</u></span></h5><hr>"
+      ),
+      paste0(
+        "<center><h4><b>$", plot_data$price,
+        "</b> / night</h4></center>"
+      ),
+      paste0("<center><h6>", popup_guests, "</h6></center>"),
+      paste0(
+        "<center><h5><b><a href=", plot_data$listing_url,
+        ">", plot_data$name, "</a></b></h5></center>"
+      ),
+      paste0(
+        "<center><img src=", plot_data$picture_url,
+        " width=300 height=180></center>"
+      )
     )
 
     # Create Leaflet map of user-filtered Singapore listings
     leaflet(data = plot_data) %>%
       addTiles(
-        urlTemplate = paste0("https://tile.jawg.io/ba3f805c-04fb-4fa7-99ef-b9",
-        "05aa38b3c8/{z}/{x}/{y}.png?access-token=eIlOZCXWfZIR2t5pqcGt6vcc25pb",
-        "scLwwCKzFgtOjISymDP6p3nvlwwLl4mA0qeH"),
+        urlTemplate = paste0(
+          "https://tile.jawg.io/ba3f805c-04fb-4fa7-99ef-b9",
+          "05aa38b3c8/{z}/{x}/{y}.png?access-token=eIlOZCXWfZIR2t5pqcGt6vcc25",
+          "pbscLwwCKzFgtOjISymDP6p3nvlwwLl4mA0qeH"
+        ),
       ) %>%
       setView(lng = 103.841959, lat = 1.3521, zoom = 11.5) %>%
       addCircles(
@@ -95,14 +113,15 @@ server <- function(input, output, session) {
         lng = ~longitude,
         stroke = FALSE,
         popup = ~popup_content,
-        color = ~palette_fn(room_type),
+        color = ~ palette_fn(room_type),
         radius = 20,
         fillOpacity = 0.5
       ) %>%
       addLegend(
         position = "bottomright",
         title = paste0(
-          "Airbnb Listings in Singapore (", filter_count, " results)"),
+          "Airbnb Listings in Singapore (", filter_count, " results)"
+        ),
         pal = palette_fn,
         values = ~room_type,
         opacity = 1
@@ -116,8 +135,10 @@ server <- function(input, output, session) {
   data_chic$price <- as.numeric(gsub("[$,]", "", data_chic$price))
 
   output$chart_chic <- renderPlotly({
-    title <- paste0(input$room_type, " listings in ",
-                    input$neighbourhood, ", Chicago")
+    title <- paste0(
+      input$room_type, " listings in ",
+      input$neighbourhood, ", Chicago"
+    )
 
     # prepares data for the chart
     chart_data <- data_chic %>%
@@ -133,27 +154,29 @@ server <- function(input, output, session) {
         )
       ) +
       scale_x_continuous(
-        breaks = seq(input$accommodation_range[1],
-                     input$accommodation_range[2],
-                     1),
-        limits = c(input$accommodation_range[1],
-                   input$accommodation_range[2])
+        breaks = seq(
+          input$accommodation_range[1],
+          input$accommodation_range[2],
+          1
+        ),
+        limits = c(
+          input$accommodation_range[1],
+          input$accommodation_range[2]
+        )
       ) +
       scale_y_continuous(
         limits = c(input$price_range[1], input$price_range[2])
       ) +
-      scale_color_gradient2(low="blue", mid="white", high="red")
-      labs(x = "Number of People Accommodating",
-           y = "Price ($USD per night)",
-           title = title
-      )
+      scale_color_gradient2(low = "blue", mid = "white", high = "red")
+    labs(
+      x = "Number of People Accommodating",
+      y = "Price ($USD per night)",
+      title = title
+    )
 
     ggplotly(scatter_plot)
   })
 
 
   ##### Interactive Page Three ################################################
-
-
-
 }
