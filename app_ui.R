5a23618bebbc38da0cdfa1ca40d8ba05345b6182
jwc225 <jwchang225@gmail.com>
2020-12-11 22:10:49 -0800

    source in required batman library for ui

--- app_ui.R
+++ app_ui.R
@@ -3,6 +3,7 @@ library("shiny")
 library("tidyverse")
 library("leaflet")
 library("plotly")
+library("batman")
 
 # Read data (setwd to root first)
 data_sing <- read.csv("data/singapore_listings.csv")
